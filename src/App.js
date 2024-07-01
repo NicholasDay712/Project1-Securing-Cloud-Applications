e5d410933d90fe9d965a17e50937b7d653b94054
Nick Day <littleporko@gmail.com>
2024-07-01 15:53:10 +1000

    Adding files first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+<link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet"/>
+<div class="header">
+  <img src="https://media.licdn.com/dms/image/D4E35AQGvmSX0n9GITQ/profile-framedphoto-shrink_200_200/0/1719443519589?e=1720414800&v=beta&t=VkQzIWX8Zs5cFQpr1neo3BdycYQtZcDW3iTD2NFwEVY" alt="pic"/>
+  <h2>Nicholas Day's Blog</h2>
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <a href='/page1'></a>
+      <div class="blogpost">
+      <h2>The Rise of Ransomware: Understanding the Threat Landscape</h2>
+      <h5>Jun, 7th 2024</h5>
+      <div class="fakeimg1">
+      </div>
+      <p></p>
+      <p>Ransomware attacks have become increasingly prevalent and sophisticated in recent years, posing significant threats to individuals, businesses, and even critical infrastructure.</p>
+    </div><a/>
+    <div class="blogpost">
+      <a href='page2.html'></a>
+        <h2>Securing Remote Work: Best Practices for a Distributed Workforce</h2>
+      <h5>Jul, 12th 2024</h5>
+      <div class="fakeimg3"></div>
+      <p></p>
+      <p>The COVID-19 pandemic has accelerated the adoption of remote work, presenting new cybersecurity challenges for organizations as employees access corporate networks and data from various locations and devices.</p>
+    </div>
+  </div><a/>
+  <div class="rightcolumn">
+    <div class="card1">
+      <h1>Hi, I'm Nic!</h1>
+      <p>Welcome to my cybersecurity blog! I'm a passionate cybersecurity student eager to share my journey and knowledge with you. Through this blog, I aim to explore the dynamic world of cybersecurity, discuss latest trends, and learn together. Join me on this exciting adventure as we delve into the complexities of digital security and strive for continuous growth one byte at a time!
+      </p>
+    </div>
+     <div class="card4">
+      <h1>Contact Information</h1>
+      <br></br>
+      Email:
+      <a href="mailto:nicholasday00@gmail.com" class="button">Send Email</a>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p class="linkedin-text">
+      LinkedIn:</p>
+      <a href="https://www.linkedin.com/in/nicholas-day-1x7" class="linkedin-button">
+    <img src="https://static.vecteezy.com/system/resources/previews/023/986/970/original/linkedin-logo-linkedin-logo-transparent-linkedin-icon-transparent-free-free-png.png" class="linkedin-logo"/>
+    </a>
+    <div class="card">
+      <h3>Popular Posts</h3>
+      <p>Securing Remote Work: Best Practices for a Distributed Workforce</p>
+      <p>The Rise of Ransomware: Understanding the Threat Landscape</p>
+    </div>
+  </div>
+</div>
+    </div></div>
+  );
+}
+
+export default App;
